9a6a87d78b9d3bf71cb012c46148ba7a617a6238
Crisstudy <kristinasaraiva12@gmail.com>
2025-01-16 16:25:38 -0300

    adcionando menu responsivo

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () { 
-    const menuToggle = document.querySelector('.menu-toggle'); 
-    const nav = document.querySelector('nav'); 
-    
-    menuToggle.addEventListener('click', function () {
-        nav.classList.toggle('active'); }); 
-        
-        document.querySelectorAll('nav a').forEach(anchor => { 
-            anchor.addEventListener('click', function (e) { 
-                e.preventDefault(); 
-                document.querySelector(this.getAttribute('href')).scrollIntoView({ 
-                    behavior: 'smooth' 
-                }); 
-                nav.classList.remove('active'); 
-            }); 
-        }); 
-    });
+//let itens = document.getElementById('itens')
+
+function clickMenu() {
+    itens.style.display = 'block'
+}
